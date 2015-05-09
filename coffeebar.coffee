6a49f53457e15b04606889fc165d9a98f0dcfd4f
Tessa Kelly <tessakkelly@gmail.com>
2015-05-08 17:19:01 -0700

    add map and reduce

--- coffeebar.coffee
+++ coffeebar.coffee
@@ -2,9 +2,23 @@ _ = {};
 
 _.each = (collection, callback) ->
   if Array.isArray collection
-    callback num for num in collection
+    callback num, ind, collection for num, ind in collection
   else
     for key, val of collection
-      callback val
+      callback val, key, collection
+
+_.map = (collection, callback) ->
+  result = []
+  _.each collection, (val, ind, list) ->
+    result.push callback val, ind, list
+  result
+
+_.reduce = (collection, accumulator, startVal) ->
+  _.each collection, (val) ->
+    if startVal
+      startVal = accumulator startVal, val
+    else 
+      startVal = val
+  startVal
 
 module.exports = _
--- spec/coffeebarSpec.js
+++ spec/coffeebarSpec.js
@@ -22,12 +22,78 @@ describe("Underbar written in coffeescript", function() {
         expect(result).toEqual(arr);
       });
 
+      it("callback has access to array and to index", function() {
+        var result = [], copyArr = [];
+        _.each(arr, function(val, ind, arr) {
+          result.push(ind);
+          copyArr.push(arr[ind]);
+        });
+        expect(result).toEqual([0,1,2,3,4]);
+        expect(copyArr).toEqual(arr);
+      });
+
       it("applies callback to each element in an object", function() {
         var result = [];
         _.each(obj, function(val) {
           result.push(val);
         });
         expect(result).toEqual(["Tessa", "San Francisco", "ABQ"]);
+      });
+
+      it("callback invoked with key & object arguments", function() {
+        var result = [], copy;
+        _.each(obj, function(val, key, object) {
+          result.push(key);
+          copy = object;
+        });
+        expect(result).toEqual(["name", "location", "hometown"]);
+        expect(copy).toEqual(obj);
+      });
+
+    });
+
+    describe("map", function() {
+
+      it("returns a new array", function() {
+        var result = _.map(arr, function(val) {
+          return val;
+        });
+        expect(result).not.toBe(arr);
+        expect(arr).toEqual([1,2,3,4,5]);
+      });
+
+      it("callback applied to each value in the array", function() {
+        var result = _.map(arr, function(val) {
+          return val * 2;
+        });
+        expect(result).toEqual([2,4,6,8,10]);
+      });
+
+      it("applies callback to each element in an object", function() {
+        var result = _.map(obj, function(val) {
+          return val + "!";
+        });
+        expect(result).toEqual(["Tessa!", "San Francisco!", "ABQ!"]);
+      });
+
+    });
+  
+
+    describe("reduce", function() {
+
+      it("combines the values of a collection according to an accumulator with a startVal", function() {
+        console.log(arr)
+        var result = _.reduce(arr, function(a, b) {
+          return a + b;
+        }, 5);
+        expect(result).toBe(20);
+      });
+
+      it("combines the values of a collection according to an accumulator without a startVal", function() {
+        var result = _.reduce(obj, function(a, b) {
+          return a + " " + b;
+        });
+        expect(result).toBe("Tessa San Francisco ABQ");
       });
 
     });
